0cfb7b84ec54508763af030ace1452e3f13a0e92
Матвей Токаревский <m.tokarevskiy@mail.ru>
2022-12-19 16:16:31 +0300

    Vkinder

---
+++ config.py
@@ -0,0 +1,12 @@
+user_token = ''
+comm_token = 'vk1.a.q9-Nrl3-xSrMHGqXgCwoIN7RfAsXO3D9AYUn3WQYxf2dWy3PZt6WWe1gXpSaP7CmD3e' \
+             '4PLFgLU0dDvvWHG7ylmUolYcdL9B_rfcxCw8UqeEkMTr2atQAAKU99pfMqRotNCQX0UNmQ' \
+             'XltsJSmpddS9dwGiVu3tsBHvi8S7_OfKt8CaFbcijIn3ucosCetMLYU_RqOB-3ucQqkSGj5BhmlPQ'
+
+offset = 0
+line = range(0, 1000)
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = 'DbyybGe['
+db_name = 'vk_bot'
